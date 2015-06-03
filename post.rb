8bccd2fe96261b9025024674ea87de34121300a2
Chris Robinson <clrobinson@users.noreply.github.com>
2015-06-03 19:02:45 +0000

    Initial commit

---
+++ post.rb
@@ -0,0 +1,18 @@
+class Post
+  
+  def initialize(title, url, points, item_id)
+    @title = title
+    @url = url
+    @points = points
+    @item_id = item_id
+  end
+
+  def comments
+    #
+  end
+
+  def add_comment(comment)
+    #
+  end
+
+end
